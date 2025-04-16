dfb4d510145f6187d9c07fc6b537e96ccdb8892e
To8a <tomassochoa.8a@gmail.com>
2025-04-16 15:43:02 -0300

    [Parte 4.4 Correción de URLConf Para funcionar]

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -14,16 +14,3 @@ urlpatterns = [
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
 
-urlpatterns = [
-    # ex: /polls/
-    path("", views.index, name="index"),
-    # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
-    # added the word 'specifics'
-    path("specifics/<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
-]
